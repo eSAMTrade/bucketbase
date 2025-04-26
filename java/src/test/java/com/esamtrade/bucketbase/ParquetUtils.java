004dd2967cfa45140b08746ebf15b52bc6d66bd7
ASU <andrei.suiu@gmail.com>
2025-04-26 21:18:26 +0300

    Removed redundant classes from ParquetUtils

--- java/src/test/java/com/esamtrade/bucketbase/ParquetUtils.java
+++ java/src/test/java/com/esamtrade/bucketbase/ParquetUtils.java
@@ -5,9 +5,6 @@ import org.apache.parquet.io.OutputFile;
 import org.apache.parquet.io.PositionOutputStream;
 import org.apache.parquet.io.SeekableInputStream;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,175 +12,6 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
 public class ParquetUtils {
-    static class SeekableByteArrayInputStream extends SeekableInputStream {
-        private final byte[] data;
-        private int pos;
-
-        SeekableByteArrayInputStream(byte[] data) {
-            this.data = data;
-            this.pos = 0;
-        }
-
-        @Override
-        public long getPos() {
-            return pos;
-        }
-
-        @Override
-        public void seek(long newPos) throws IOException {
-            if (newPos < 0 || newPos > data.length) {
-                throw new IOException("Invalid seek: " + newPos);
-            }
-            pos = (int) newPos;
-        }
-
-        @Override
-        public int read() {
-            if (pos >= data.length)
-                return -1;
-            return data[pos++] & 0xFF;
-        }
-
-        @Override
-        public int read(byte[] b, int off, int len) {
-            if (pos >= data.length)
-                return -1;
-            int toRead = Math.min(len, data.length - pos);
-            System.arraycopy(data, pos, b, off, toRead);
-            pos += toRead;
-            return toRead;
-        }
-
-        @Override
-        public void readFully(byte[] b) throws IOException {
-            readFully(b, 0, b.length);
-        }
-
-        @Override
-        public void readFully(byte[] b, int off, int len) throws IOException {
-            int remaining = len;
-            int offset = off;
-            while (remaining > 0) {
-                int r = read(b, offset, remaining);
-                if (r < 0)
-                    throw new EOFException("EOF before filling buffer");
-                remaining -= r;
-                offset += r;
-            }
-        }
-
-        @Override
-        public int read(ByteBuffer buf) throws IOException {
-            if (!buf.hasRemaining())
-                return 0;
-            int toRead = Math.min(buf.remaining(), data.length - pos);
-            if (toRead <= 0)
-                return -1;
-            buf.put(data, pos, toRead);
-            pos += toRead;
-            return toRead;
-        }
-
-        @Override
-        public void readFully(ByteBuffer buf) throws IOException {
-            int remaining = buf.remaining();
-            byte[] temp = new byte[remaining];
-            readFully(temp, 0, remaining);
-            buf.put(temp);
-        }
-    }
-
-    static class ByteArrayPositionOutputStream extends PositionOutputStream {
-        private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        @Override
-        public void write(int b) throws IOException {
-            baos.write(b);
-        }
-
-        @Override
-        public void write(byte[] b, int off, int len) throws IOException {
-            baos.write(b, off, len);
-        }
-
-        @Override
-        public long getPos() {
-            return baos.size();
-        }
-
-        byte[] toByteArray() {
-            return baos.toByteArray();
-        }
-    }
-
-    static class InMemoryOutputFile implements OutputFile {
-        private ByteArrayPositionOutputStream current;
-
-        @Override
-        public PositionOutputStream create(long blockSizeHint) {
-            current = new ByteArrayPositionOutputStream();
-            return current;
-        }
-
-        @Override
-        public PositionOutputStream createOrOverwrite(long blockSizeHint) {
-            return create(blockSizeHint);
-        }
-
-        @Override
-        public boolean supportsBlockSize() {
-            return false;
-        }
-
-        @Override
-        public long defaultBlockSize() {
-            return 0;
-        }
-
-        byte[] toByteArray() {
-            return current.toByteArray();
-        }
-    }
-
-    static class InMemoryInputFile implements InputFile {
-        private final byte[] data;
-
-        InMemoryInputFile(byte[] data) {
-            this.data = data;
-        }
-
-        @Override
-        public long getLength() {
-            return data.length;
-        }
-
-        @Override
-        public SeekableInputStream newStream() {
-            return new SeekableByteArrayInputStream(data);
-        }
-    }
-
-    static class BufferedBytesInputFile implements InputFile {
-        private final byte[] data;
-
-        BufferedBytesInputFile(byte[] data) {
-            this.data = data;
-        }
-
-        @Override
-        public long getLength() {
-            return data.length;
-        }
-
-        @Override
-        public SeekableInputStream newStream() {
-            ByteArrayInputStream bais = new ByteArrayInputStream(data);
-            BufferedInputStream bis = new BufferedInputStream(bais);
-            SeekableInputStream sis = new SeekableInputStreamWrapper(bis);
-            return sis;
-        }
-    }
-
     static class StreamInputFile implements InputFile {
         private final ObjectStream stream;
 
@@ -311,21 +139,25 @@ public class ParquetUtils {
             this.out = out;
         }
 
-        @Override public void write(int b) throws IOException {
+        @Override
+        public void write(int b) throws IOException {
             out.write(b);
             pos++;
         }
 
-        @Override public void write(byte[] b, int off, int len) throws IOException {
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
             out.write(b, off, len);
             pos += len;
         }
 
-        @Override public long getPos() {
+        @Override
+        public long getPos() {
             return pos;
         }
 
-        @Override public void close() throws IOException {
+        @Override
+        public void close() throws IOException {
             out.close();
         }
     }
@@ -348,7 +180,14 @@ public class ParquetUtils {
             return posOut;
         }
 
-        @Override public boolean supportsBlockSize() { return false; }
-        @Override public long defaultBlockSize()   { return 0; }
+        @Override
+        public boolean supportsBlockSize() {
+            return false;
+        }
+
+        @Override
+        public long defaultBlockSize() {
+            return 0;
+        }
     }
 }
